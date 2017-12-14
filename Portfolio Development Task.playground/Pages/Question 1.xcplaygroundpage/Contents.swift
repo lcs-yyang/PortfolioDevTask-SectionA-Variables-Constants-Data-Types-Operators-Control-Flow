a78b56df4ccb2cc6038b443f4f5f3eb829d217e8
Russell Gordon <rgordon@lcs.on.ca>
2017-12-14 07:17:57 -0500

    Question 1 complete (naming conventions and camel casing variable names).

---
+++ Portfolio Development Task.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+/*:
+ # Question 1
+
+ Several variables are declared below and then used to calculate the distance a car has travelled.
+ 
+ Change the variable names so they are more descriptive of what they keep track of.
+ 
+ Use the camel case convention.
+ */
+// Answer question 1 below
+var howlongweweretravellingfor = 5.25
+var s = 80.0
+let dct = s * howlongweweretravellingfor
+
+/*:
+- callout(Shortcut): The **Edit All in Scope** menu command is useful when renaming variables – place your cursor within the variable name and then invoke the menu command.
+ 
+ ![edit_all_in_scope](editallinscope.png "Edit All in Scope")
+ 
+ ## Now share your understanding
+ 
+ 1. Commit your response on this page (Option-Command-C).
+ 2. [Add a link][al] to your Computer Science portfolio.
+ 
+ [al]:
+ https://www.youtube.com/watch?v=Wa3Wl3T25jo&list=PLTGGOQnktyWs9TlNJ30pgYgypvIGrt3Lx&index=1
+ 
+ ### Learning Goals - Software Development
+ 
+ * Goal 8
+    * *Communication*
+        * I can write human-readable programs (naming, indenting) with appropriate documentation (comments).
+
+ [Next](@next)
+ */
