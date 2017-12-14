67ab590228e5467e9329da52e4493f67b1e42411
Russell Gordon <rgordon@lcs.on.ca>
2017-12-14 06:47:00 -0500

    Initial Commit

---
+++ PortfolioDevTask-SectionA-Variables-Constants-Data-Types-Operators-Control-Flow/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PortfolioDevTask-SectionA-Variables-Constants-Data-Types-Operators-Control-Flow
+//
+//  Created by Russell Gordon on 12/14/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
