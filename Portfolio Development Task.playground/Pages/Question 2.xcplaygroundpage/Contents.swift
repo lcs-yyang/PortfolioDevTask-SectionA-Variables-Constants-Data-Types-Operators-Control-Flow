34b9bae84ae05619b55f8dcaa021fb5131c991b3
Russell Gordon <rgordon@lcs.on.ca>
2017-12-14 07:28:12 -0500

    Added question 2. Variables vs constants.

---
+++ Portfolio Development Task.playground/Pages/Question 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+/*:
+ # Question 2
+ 
+ Imagine you have a side project as a musician.
+ 
+ You were signed by a label and just released your first album.
+ 
+ You are not sure how the album will be received but are hopeful that many people will appreciate your vocals and skill with a ukelele.
+ 
+ Create and initialize a variable or constant (whatever is more appropriate), using a reasonable data type, to represent each of these items:
+ 
+ * name of the album
+ * copies sold
+ * suggested selling price
+ 
+ */
+// Answer question 2 below
+
+/*:
+ ## Now share your understanding
+ 
+ 1. Commit your response on this page (Option-Command-C).
+ 2. [Add a link][al] to your Computer Science portfolio.
+ 
+ [al]:
+ https://www.youtube.com/watch?v=Wa3Wl3T25jo&list=PLTGGOQnktyWs9TlNJ30pgYgypvIGrt3Lx&index=1
+ 
+ ### Learning Goals - Programming
+ 
+ * Goal 1
+    * *Knowledge*
+        * I know the difference between a constant and a variable and when it is appropriate to use each.
+ 
+ [Next](@next)
+ */
+
