9292d9b769d7b37837d90dc2e24f32519807827c
Russell Gordon <rgordon@lcs.on.ca>
2017-12-14 07:35:20 -0500

    Fixed typo

--- Portfolio Development Task.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@
  
  **Code that is not documented in your portfolio cannot and will not be considered in determining your grade for the course.**
  
- To be clear (though I'd hope this is unlikely) students who do not regularly document their understanding using their Computer Science portfolio will receive an "I" for "insufficient evidence" on reports and are at risk of not earning the credit in this course. Prior to that occuring (which again I'd hope is unlikely) I would have conversations with the student(s) in question and you would be referred to supervised study (undocumented work in Computer Science will be handled the same as late and missing work in other subjects).
+ To be clear (though I'd hope this is unlikely) students who do not regularly document their understanding using their Computer Science portfolio will receive an "I" for "insufficient evidence" on reports and are at risk of not earning the credit in this course. Prior to that occurring (which again I'd hope is unlikely) I would have conversations with the student(s) in question and you would be referred to supervised study (undocumented work in Computer Science will be handled the same as late and missing work in other subjects).
  
  Once again, if you forget how to add links to your portfolio, [here is a video explaining the process that was shared earlier this year][al].
  
