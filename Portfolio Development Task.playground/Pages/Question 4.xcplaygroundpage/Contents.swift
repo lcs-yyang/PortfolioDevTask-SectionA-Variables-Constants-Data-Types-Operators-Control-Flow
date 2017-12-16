004479018a15cc84719f546dbd68da80ee39a833
Yang, Yuxin <yyang@lcs-mba3025.lcs.on.ca>
2017-12-16 11:38:13 -0500

    Done question 4

--- Portfolio Development Task.playground/Pages/Question 4.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 4.xcplaygroundpage/Contents.swift
@@ -19,7 +19,19 @@
  
  */
 // Answer question 4 below
+var moneyIHaveEarned = 0
 
+// 1 bad joke challenge
+moneyIHaveEarned = moneyIHaveEarned + 5
+
+// 2 rap song challange
+moneyIHaveEarned = moneyIHaveEarned - 2
+
+// 3 singing song challenge
+moneyIHaveEarned = moneyIHaveEarned * 4
+
+// 4 waving car challenge
+moneyIHaveEarned = Int(12 - (0.75 * 13))
 /*:
  ## Now share your understanding
  
